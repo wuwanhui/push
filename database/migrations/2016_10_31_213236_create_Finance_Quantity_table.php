c8688bb5e13f6513a2607df41c5f8ba0e263229e
cqyuanyou <wuhong@yeah.net>
2016-11-01 09:54:26 +0800

    2016-11-1

---
+++ database/migrations/2016_10_31_213236_create_Finance_Quantity_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFinanceQuantityTable extends Migration
+{
+    /**
+     * 充值记录
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Finance_Quantity', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('userId');//充值者
+            $table->integer('quantity');//数量
+            $table->integer('rechargeId')->default(0);//充值记录
+            $table->integer('direction')->default(0);//收支方向0收入1支出
+            $table->integer('liableId')->default(0);//责任人
+            $table->integer('state')->default(1);//状态0成功1待审核2失败
+            $table->date('expiryDate')->nullable();//有效期止
+            $table->integer('sort')->default(0);//排序
+            $table->text('remark')->nullable();//备注
+            $table->softDeletes();
+            $table->timestamps();
+
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('Finance_Quantity');
+    }
+}
